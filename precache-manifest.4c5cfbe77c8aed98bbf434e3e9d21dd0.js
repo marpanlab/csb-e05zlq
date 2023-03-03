6c863966ae34521b67a9a5fb9442515b62111fc8
Code Sandbox <hello@codesandbox.io>
2023-03-03 00:56:47 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4c5cfbe77c8aed98bbf434e3e9d21dd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd30169f8835f3562955de8cc4a94cad",
+    "url": "https://marpanlab.github.io/csb-e05zlq/index.html"
+  },
+  {
+    "revision": "49302df82a2098624d5e",
+    "url": "https://marpanlab.github.io/csb-e05zlq/static/js/2.d998e913.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "https://marpanlab.github.io/csb-e05zlq/static/js/2.d998e913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16548669d4c8ee59bebd",
+    "url": "https://marpanlab.github.io/csb-e05zlq/static/js/main.8abcef7f.chunk.js"
+  },
+  {
+    "revision": "3b104665a593b1553309",
+    "url": "https://marpanlab.github.io/csb-e05zlq/static/js/runtime-main.f7030c48.js"
+  }
+]);
